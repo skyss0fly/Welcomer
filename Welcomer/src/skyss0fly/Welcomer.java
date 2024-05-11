989b19b3626237140d435035c564091e2eeece89
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-05-11 10:00:40 +1000

    Create Welcomer.java

---
+++ Welcomer/src/skyss0fly/Welcomer.java
@@ -0,0 +1,12 @@
+package skyss0fly.Welcomer;
+
+
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukki.event.plugin.PluginBase;
+
+class Main extends PluginBase(){
+public void onJoin(PlayerJoinEvent event){
+
+  }
+
+}
