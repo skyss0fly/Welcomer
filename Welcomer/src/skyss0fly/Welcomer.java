7e3ae5bd84c923a4cefbf533089bcbec2a649505
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-05-11 10:04:09 +1000

    Update Welcomer.java

--- Welcomer/src/skyss0fly/Welcomer.java
+++ Welcomer/src/skyss0fly/Welcomer.java
@@ -6,7 +6,8 @@ import org.bukki.event.plugin.PluginBase;
 
 class Main extends PluginBase(){
 public void onJoin(PlayerJoinEvent event){
-
+  name event().getName();
+this.getServer().broadcastMessage(name , " has Joined the Server");
   }
 
 }
