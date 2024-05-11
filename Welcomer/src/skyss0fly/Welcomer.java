4cec46739d2b863621e0077655dd501f1a29a678
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-05-11 10:05:24 +1000

    Update Welcomer.java

--- Welcomer/src/skyss0fly/Welcomer.java
+++ Welcomer/src/skyss0fly/Welcomer.java
@@ -1,13 +1,20 @@
 package skyss0fly.Welcomer;
 
-
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukki.event.plugin.PluginBase;
+import org.bukkit.plugin.java.JavaPlugin;
 
-class Main extends PluginBase(){
-public void onJoin(PlayerJoinEvent event){
-  name event().getName();
-this.getServer().broadcastMessage(name , " has Joined the Server");
-  }
+public class Main extends JavaPlugin implements Listener {
 
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        String playerName = event.getPlayer().getName();
+        getServer().broadcastMessage(playerName + " has joined the server!");
+    }
 }
